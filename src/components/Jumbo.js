1311b73df1c1cc2ff2f5c27d80bdad06eb1de863
enderta <endertanver@gmail.com>
2023-06-07 18:41:24 +0100

    jobs

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,58 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import { useTransition, animated, AnimatedProps } from '@react-spring/web';
-
-import styles from './jumbo.css';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Container } from 'react-bootstrap';
 
 function Jumbo() {
     const [jobs, setJobs] = useState([]);
-    const [index, setIndex] = useState(0);
-
-    const onClick = () => setIndex((state) => (state + 1) % 3);
-
-    const transitions = useTransition(index, {
-        from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
-        enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-        leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
-    });
 
     useEffect(() => {
-        async function getJobs() {
-            try {
-                const res = await fetch('http://localhost:5000/api/jobs');
-                const data = await res.json();
+        fetch('http://localhost:5000/api/jobs')
+            .then((res) => res.json())
+            .then((data) => {
                 setJobs(data.data);
-            } catch (error) {
-                console.log('Error fetching jobs:', error);
-            }
-        }
-        getJobs();
+            });
     }, []);
 
-
-    const pages = [
-        ({ style }) => (
-            <animated.div style={{ ...style, background: 'lightpink' }}>{jobs[0]?.title}</animated.div>
-        ),
-        ({ style }) => (
-            <animated.div style={{ ...style, background: 'lightblue' }}>{jobs[1]?.title}</animated.div>
-        ),
-        ({ style }) => (
-            <animated.div style={{ ...style, background: 'lightgreen' }}>{jobs[2]?.title}</animated.div>
-        ),
-    ];
-
     return (
-       <div >
-           <div className="container">
-                   <div  onClick={onClick}>
-                       {transitions((style, i) => {
-                           const Page = pages[i];
-                           return <Page style={style} />;
-                       })}
-                   </div>
-           </div>
-       </div>
+        <div style={{ margin: '10px' }}>
+            <div
+                className="jumbotron jumbotron-fluid"
+                style={{
+                    position: 'relative',
+                    width: '100%', // Adjust the width as desired
+                    height: '500px', // Adjust the height as desired
+                    borderRadius: '0', // Remove rounded corners for a square shape
+                }}
+            >
+                <div
+                    className="jumbotron-background"
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        backgroundImage: 'url("https://picsum.photos/200/300")',
+                        backgroundSize: 'cover',
+                        opacity: 0.5, // Adjust the opacity as desired
+                        zIndex: -1,
+                    }}
+                ></div>
+                <Container>
+                    <h1 className="display-4">Jumbotron Heading</h1>
+                    <p className="lead">Jumbotron Subtitle</p>
+                    <Carousel
+                        infiniteLoop
+                        showThumbs={false}
+                        showStatus={false}
+                        showIndicators={false}
+                        showArrows={false}
+                        interval={3000}
+                        centerMode={true}
+centerSlidePercentage={100}
+                        stopOnHover={true}
+                        autoPlay={true}
 
+                    >
+                        {jobs.map((job) => (
+                            <div key={job.id}>
+                                <h3>{job.title}</h3>
+                                <h4>{job.company}</h4>
+                                <p>{job.location}</p>
+                                <p>{job.description}</p>
+                            </div>
+                        ))}
+                    </Carousel>
+                </Container>
+            </div>
+        </div>
     );
 }
 
