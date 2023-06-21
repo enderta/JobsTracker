2093f78889066476567a82b7ecad94fc69521604
enderta <endertanver@gmail.com>
2023-06-21 12:15:33 +0100

    commerce

--- src/components/commerce/NaviBar.js
+++ src/components/commerce/NaviBar.js
@@ -50,7 +50,7 @@ function NaviBar(props) {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
 
-                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="/home">Home</Nav.Link>
                             <Nav.Link href="#link">Link</Nav.Link>
 
                         </Nav>
