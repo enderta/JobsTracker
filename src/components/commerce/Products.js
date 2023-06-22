790fa5dab02698afa8aba68be5e5dc53de30c735
enderta <endertanver@gmail.com>
2023-06-22 12:38:03 +0100

    commerce

--- src/components/commerce/Products.js
+++ src/components/commerce/Products.js
@@ -120,7 +120,7 @@ function Products() {
                                 variant={darkMode ? 'outline-warning' : 'outline-dark'}
                                 onClick={() => handleAddToBasket(product, localStorage.getItem('userId'))}
                             >
-                                Add to basket
+                                Add to basket 🛒
                             </Button>
                         </Card.Footer>
                     </Card>
