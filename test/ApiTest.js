250030c0b70e3212dbbc0105ce44ac9aa74bf05d
enderta <endertanver@gmail.com>
2023-06-24 10:56:35 +0100

    commerce

--- test/ApiTest.js
+++ test/ApiTest.js
@@ -4,8 +4,20 @@ describe('api testing', () => {
         // eslint-disable-next-line no-undef
         browser.url('https://www.amazon.com/');
         // eslint-disable-next-line no-undef
+        browser.window.fullscreen();
+        // eslint-disable-next-line no-undef
+        //search for a product
+        // eslint-disable-next-line no-undef
+       browser.findElement("xpath", "//input[@id='twotabsearchtextbox']")
+            .sendKeys("iphone 12 pro max");
+        // eslint-disable-next-line no-undef
+        browser.findElement("xpath", "//input[@id='nav-search-submit-button']")
+            .click();
+
+
+
         const title = browser.getTitle();
         console.log(title);
-        expect(title).to.equal('Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more');
+        //expect(title).to.equal('Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more');
     });
 });
