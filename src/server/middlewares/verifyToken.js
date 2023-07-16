f080d31add946ae92f222e57c4d875d444cc2bc9
enderta <endertanver@gmail.com>
2023-07-17 00:23:29 +0100

    api

--- src/server/middlewares/verifyToken.js
+++ src/server/middlewares/verifyToken.js
@@ -1,8 +1,11 @@
 const jwt = require("jsonwebtoken");
-const secret = "secret";
+
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const verifyToken = (req, res, next) => {
-    jwt.verify(req.headers.authorization, secret, (error, decoded) => {
+    jwt.verify(req.headers.authorization, process.env.SECRET, (error, decoded) => {
         if(error) {
             res.status(401).json({error: "Unauthorized"});
         } else {
