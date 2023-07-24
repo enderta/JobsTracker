8895759e5f21ada84d770b8b81718d093da23802
enderta <endertanver@gmail.com>
2023-07-24 14:31:59 +0100

    Refactor job state and clean up code

--- src/components/jobtracker/AddJobs.js
+++ src/components/jobtracker/AddJobs.js
@@ -1,131 +1,99 @@
-import React, {useState} from 'react'
-import {Button, Form, Modal} from "react-bootstrap";
+import React, {useState} from 'react';
+import {Button, Form, Modal} from 'react-bootstrap';
 
 function AddJobs(props) {
-    const [title, setTitle] = useState("");
-    const [company, setCompany] = useState("");
-    const [location, setLocation] = useState("");
-    const [description, setDescription] = useState("");
-    const [requirements, setRequirements] = useState("");
-    const [is_applied, setIs_applied] = useState(false);
-    const [posted_at, setPosted_at] = useState("");
-    const [updated_at, setUpdated_at] = useState("");
-    const [user_id, setUser_id] = useState(localStorage.getItem('user_id'));
+    const [job, setJob] = useState({
+        title: '',
+        company: '',
+        location: '',
+        description: '',
+        requirements: '',
+        is_applied: false,
+        posted_at: '',
+        updated_at: '',
+        user_id: localStorage.getItem('user_id'),
+    });
 
     const onSubmitForm = async (e) => {
         e.preventDefault();
         try {
-
-            const body = {
-                title,
-                company,
-                location,
-                description,
-                requirements,
-                is_applied,
-                posted_at,
-                updated_at,
-                user_id
-            };
-            const response = await fetch("http://localhost:5000/api/jobs/createJob/" + user_id, {
-                method: "POST",
+            const response = await fetch('http://localhost:5000/api/jobs/createJob/' + job.user_id, {
+                method: 'POST',
                 headers: { 'Content-Type': 'application/json', Authorization: localStorage.getItem('token') },
-                body: JSON.stringify(body)});
+                body: JSON.stringify(job),
+            });
             console.log(response);
             alert('Job added successfully');
-
         } catch (err) {
             console.error(err.message);
         }
         window.location.reload();
-    }
+    };
 
-    const handleChanges = (e) => {
-        if (e.target.name === "title") {
-            setTitle(e.target.value)
-        } else if (e.target.name === "company") {
-            setCompany(e.target.value)
-        } else if (e.target.name === "location") {
-            setLocation(e.target.value)
-        } else if (e.target.name === "description") {
-            setDescription(e.target.value)
-        } else if (e.target.name === "requirements") {
-            setRequirements(e.target.value)
-        } else if (e.target.name === "is_applied") {
-            setIs_applied(e.target.value)
-        } else if (e.target.name === "posted_at") {
-            setPosted_at(e.target.value)
-        } else if (e.target.name === "updated_at") {
-            setUpdated_at(e.target.value)
-        } else if (e.target.name === "user_id") {
-            setUser_id(e.target.value)
-        }
-    }
+    const handleChanges = (e) => setJob({...job, [e.target.name]: e.target.value});
 
     return (
-        <div>
-            <div className="container">
+        <div className="container">
+            <Modal show={props.show} onHide={props.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        <h3 style={{color: "black"}}>Add Job</h3>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Title" name="title"
+                                          data-testid="Enter Title"
+                                          value={job.title}
+                                          onChange={handleChanges}/>
+                        </Form.Group>
 
-                <Modal show={props.show} onHide={props.handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>
-                            <h3 style={{color: "black"}}>Add Job</h3>
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form >
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Title</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Title" name="title"
-                                              data-testid="Enter Title"
-                                              value={title}
-                                              onChange={handleChanges}/>
-                            </Form.Group>
-
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Company</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Company" name="company"
-                                              data-testid="Enter Company"
-                                              value={company}
-                                              onChange={handleChanges}/>
-                            </Form.Group>
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Location</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Location" name="location"
-                                              value={location}
-                                              data-testid="Enter Location"
-                                              onChange={handleChanges}/>
-                            </Form.Group>
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Description</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Description" name="description"
-                                              value={description}
-                                                data-testid="Enter Description"
-                                              onChange={handleChanges}/>
-                            </Form.Group>
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Requirements</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Requirements" name="requirements"
-                                              value={requirements}
-                                                data-testid="Enter Requirements"
-                                              onChange={handleChanges}/>
-                            </Form.Group>
-                        </Form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={props.handleClose}>
-                            Close
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Company</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Company" name="company"
+                                          data-testid="Enter Company"
+                                          value={job.company}
+                                          onChange={handleChanges}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Location</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Location" name="location"
+                                          value={job.location}
+                                          data-testid="Enter Location"
+                                          onChange={handleChanges}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Description" name="description"
+                                          value={job.description}
+                                          data-testid="Enter Description"
+                                          onChange={handleChanges}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Requirements</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Requirements" name="requirements"
+                                          value={job.requirements}
+                                          data-testid="Enter Requirements"
+                                          onChange={handleChanges}/>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.handleClose}>
+                        Close
+                    </Button>
+                    <div>
+                        <Button variant="primary" onClick={onSubmitForm}
+                                data-testid={"submit-button"}>
+                            Save Changes
                         </Button>
-                        <div>
-                            <Button variant="primary" onClick={onSubmitForm}
-                                    data-testid={"submit-button"}>
-                                Save Changes
-                            </Button>
-                        </div>
-                    </Modal.Footer>
-                </Modal>
-            </div>
-        </div>)
+                    </div>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
 }
 
-export default AddJobs
+export default AddJobs;
--- src/components/jobtracker/Cards.js
+++ src/components/jobtracker/Cards.js
@@ -19,13 +19,13 @@ function Cards(props) {
     const fetchJobs = () => {
         let apiUrl = `http://localhost:5000/api/jobs/${localStorage.getItem('user_id')}?search=${search}`;
 
-        fetch(apiUrl,{
+        fetch(apiUrl, {
             method: 'GET',
-            headers: { 'Content-Type': 'application/json', Authorization: localStorage.getItem('token') },
+            headers: {'Content-Type': 'application/json', Authorization: localStorage.getItem('token')},
         })
             .then((res) => res.json())
             .then((data) => {
-                data.status==='success' ? setData(data.data) : setData([]);
+                data.status === 'success' ? setData(data.data) : setData([]);
             })
             .catch((err) => {
                 console.log(err);
@@ -41,7 +41,8 @@ function Cards(props) {
 
         fetch(`http://localhost:5000/api/jobs/${localStorage.getItem('user_id')}/${id}`, {
             method: 'PATCH',
-            headers: { 'Content-Type': 'application/json', Authorization: localStorage.getItem('token')
+            headers: {
+                'Content-Type': 'application/json', Authorization: localStorage.getItem('token')
             }, body: JSON.stringify({
                 is_applied: !isApplied, updated_at: new Date().toISOString(),
             }),
@@ -57,80 +58,80 @@ function Cards(props) {
 
     return (<div>
 
-                <div style={{marginTop: '10px', marginBottom: '10px'}}>
-                  <Filters data={data}
-                           value={search}
-                            search={search}
-                           handleSearch={handleSearch}/>
-                </div>
+        <div style={{marginTop: '10px', marginBottom: '10px'}}>
+            <Filters data={data}
+                     value={search}
+                     search={search}
+                     handleSearch={handleSearch}/>
+        </div>
 
-                <div className="row"  data-testid="cards-component">
-                    {data && data.length > 0
-                        ? data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
-                            .map((job) => (<div key={job.id} className="col-md-3 mb-3">
-                                <Card
-                                    data-testid="cards-component"
-                                    style={{backgroundColor: props.dark ? '#070f23' : 'white'}}
+        <div className="row" data-testid="cards-component">
+            {data && data.length > 0
+                ? data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
+                    .map((job) => (<div key={job.id} className="col-md-3 mb-3">
+                        <Card
+                            data-testid="cards-component"
+                            style={{backgroundColor: props.dark ? '#070f23' : 'white'}}
 
+                        >
+                            <Card.Body style={{height: '200px', width: '400px'}}>
+                                <Card.Title>
+                                    {job.title}
+                                </Card.Title>
+                                <Card.Subtitle
+                                    className="mb-2 text-muted"
                                 >
-                                    <Card.Body style={{height: '200px', width: '400px'}}>
-                                        <Card.Title>
-                                            {job.title}
-                                        </Card.Title>
-                                        <Card.Subtitle
-                                            className="mb-2 text-muted"
-                                        >
-                                            {job.company}
-                                        </Card.Subtitle>
-                                        <Card.Text //read all the on mouse hover in spreate box
-                                            onMouseOver={(e) => {
-                                                e.target.style.cursor = 'pointer';
-                                                e.target.title = job.description;
-                                            }}
-                                        >
-                                            {job.description.split(' ').length > 3 ? `${job.description.split(' ').slice(0, 4).join(' ')} ...` : job.description}
+                                    {job.company}
+                                </Card.Subtitle>
+                                <Card.Text //read all the on mouse hover in spreate box
+                                    onMouseOver={(e) => {
+                                        e.target.style.cursor = 'pointer';
+                                        e.target.title = job.description;
+                                    }}
+                                >
+                                    {job.description.split(' ').length > 3 ? `${job.description.split(' ').slice(0, 4).join(' ')} ...` : job.description}
 
-                                        </Card.Text>
-                                        <Card.Text>
-                                            {job.location}
-                                        </Card.Text>
-                                        <Card.Text
-                                            onMouseOver={(e) => {
-                                                e.target.style.cursor = 'pointer';
-                                                e.target.title = job.requirements;
-                                            }}
-                                        >
-                                            {job.requirements.split(' ').length > 3 ? `${job.requirements.split(' ').slice(0, 4).join(' ')} ...` : job.requirements}
-                                        </Card.Text>
+                                </Card.Text>
+                                <Card.Text>
+                                    {job.location}
+                                </Card.Text>
+                                <Card.Text
+                                    onMouseOver={(e) => {
+                                        e.target.style.cursor = 'pointer';
+                                        e.target.title = job.requirements;
+                                    }}
+                                >
+                                    {job.requirements.split(' ').length > 3 ? `${job.requirements.split(' ').slice(0, 4).join(' ')} ...` : job.requirements}
+                                </Card.Text>
 
 
-                                        <Card.Text>
-                                            <h6
-                                                cy-data="applied-at"
-                                                onClick={() => handleCheck(job.id, job.is_applied)}
-                                                style={{
-                                                    color: job.is_applied ? 'forestgreen' : 'goldenrod',
-                                                }}
-                                            >
+                                <Card.Text>
+                                    <h6
+                                        cy-data="applied-at"
+                                        onClick={() => handleCheck(job.id, job.is_applied)}
+                                        style={{
+                                            color: job.is_applied ? 'forestgreen' : 'goldenrod',
+                                        }}
+                                    >
 
-                                                {job.is_applied ? "Applied At: " + new Date(job.updated_at)
-                                                    .toString()
-                                                    .split(' ')
-                                                    .slice(0, 4)
-                                                    .join(' ') : "Click to Apply"}
-                                            </h6>
-                                        </Card.Text>
-                                    </Card.Body>
-                                    <br/>
-                                    <Card.Footer>
-                                     <Delete  id={job.id} />
-                                    </Card.Footer>
-                                </Card>
+                                        {job.is_applied ? "Applied At: " + new Date(job.updated_at)
+                                            .toString()
+                                            .split(' ')
+                                            .slice(0, 4)
+                                            .join(' ') : "Click to Apply"}
+                                    </h6>
+                                </Card.Text>
+                            </Card.Body>
+                            <br/>
+                            <Card.Footer>
+                                <Delete id={job.id}/>
+                            </Card.Footer>
+                        </Card>
 
-                            </div>)):[]}
-                </div>
+                    </div>)) : []}
+        </div>
 
-        </div>);
+    </div>);
 }
 
 export default Cards;
