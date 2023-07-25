ac7908d68a2dc91d0ac6002739e4654259b925f1
enderta <endertanver@gmail.com>
2023-07-25 13:22:28 +0100

    Refactor conditional route rendering in Pages component

--- src/components/jobtracker/Pages.js
+++ src/components/jobtracker/Pages.js
@@ -5,20 +5,19 @@ import Register from "./Register";
 import Login from "./Login";
 import HomePage from "./HomePage";
 
-
 function Pages() {
+    const isLoggedIn = localStorage.getItem("token");
+
     return (
         <div>
-
             <Routes>
                 <Route path={"/*"} element={<HomePage/>}/>
-                {localStorage.getItem("token") ? <Route path="/jobs" element={<Jumbo/> }/> : <Route path="/jobs" element={<Login/>}/> }
+                <Route path="/jobs" element={isLoggedIn ? <Jumbo/> : <Login/>}/>
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/login" element={<Login/>}/>
             </Routes>
-
         </div>
-    )
+    );
 }
 
-export default Pages
+export default Pages;
