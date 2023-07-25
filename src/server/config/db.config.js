ceb7aed876f2aebb521003185a978c9e0cca2e97
enderta <endertanver@gmail.com>
2023-07-25 18:59:33 +0100

    Added SSL configuration to database settings
    Database configuration has been updated to include SSL settings. This secure connection is necessary for databases that require SSL, increasing overall application security.

--- src/server/config/db.config.js
+++ src/server/config/db.config.js
@@ -9,6 +9,9 @@ host: process.env.DB_HOST,
 database: process.env.DB_DATABASE,
 password: process.env.DB_PASSWORD,
 port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 pool.connect(function(err) {
