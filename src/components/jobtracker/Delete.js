42ef704e513522678df70d5c8373149952e09e2f
enderta <endertanver@gmail.com>
2023-07-25 13:18:11 +0100

    Refactor delete job method for readability

--- src/components/jobtracker/Delete.js
+++ src/components/jobtracker/Delete.js
@@ -3,31 +3,33 @@ import {Button} from "react-bootstrap";
 
 const Delete = (props) => {
     const handleDelete = (id) => {
+        const apiUrl = `http://localhost:5000/api/jobs/${localStorage.getItem('user_id')}/${id}`;
+        const headers = {
+            'Content-Type': 'application/json',
+            Authorization: localStorage.getItem('token')
+        };
 
-        fetch(`http://localhost:5000/api/jobs/${localStorage.getItem('user_id')}/${id}`, {
+        fetch(apiUrl, {
             method: 'DELETE',
-            headers: { 'Content-Type': 'application/json', Authorization: localStorage.getItem('token') },
+            headers: headers,
         })
             .then((res) => res.json())
-
             .then((data) => {
                 console.log(data);
                 alert('Job deleted successfully');
-               window.location.reload();
+                window.location.reload();
             })
             .catch((err) => {
                 console.log(err);
             });
     };
 
-
-
     return (
         <div>
             <Button
                 data-testid="delete-button"
-                variant={ 'outline-danger'}
-                data-cy={'delete-button'}
+                variant='outline-danger'
+                data-cy='delete-button'
                 onClick={() => handleDelete(props.id)}
             >
                 Delete
