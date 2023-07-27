65682063a855782e017a8168115f71d3c852e6b7
enderta <endertanver@gmail.com>
2023-07-27 18:54:17 +0100

    Update API URL in EditJob component
    Changed apiUrl in EditJob component to use the deployed backend of localhost. This is necessary for the job editing capability to work on production.

--- src/server/modules/jobs/jobs.service.js
+++ src/server/modules/jobs/jobs.service.js
@@ -1,135 +1,61 @@
 const pool = require("../../config/db.config");
 
-
-const getJobs = async (req, res) => {
-    try {
-        const searchTerm = req.query?.search || '';
-        const user_id = req.params?.user_id;
-
-        if (!searchTerm && user_id) {
-            const jobs = await pool.query("SELECT * FROM jobs WHERE user_id = $1", [
-                user_id,
-            ]);
-            res.json({
-                status: "success",
-                message: `Retrieved ${jobs.rows.length} jobs`,
-                data: jobs.rows
-            });
-        } else if (searchTerm && user_id) {
-            const jobs = await pool.query(
-                `SELECT *
-                 FROM jobs
-                 WHERE title ILIKE $1
-                    OR company ILIKE $1
-                    OR location ILIKE $1
-                    OR description ILIKE $1
-                    OR requirements ILIKE $1
-                   AND user_id = $2`,
-                [`%${searchTerm}%`, user_id],
-            );
-            res.json({
-                status: "success",
-                message: `Retrieved ${jobs.rows.length} jobs`,
-                data: jobs.rows
-            });
-        }
-    } catch (err) {
-        res.status(500).json({message: err.message});
+const getJobs = async (userId, searchTerm = '') => {
+    if (!searchTerm && userId) {
+        const jobs = await pool.query("SELECT * FROM jobs WHERE user_id = $1", [
+            userId,
+        ]);
+        return jobs.rows;
+    } else if (searchTerm && userId) {
+        const jobs = await pool.query(
+            `SELECT *
+             FROM jobs
+             WHERE (title ILIKE $1
+                 OR company ILIKE $1
+                 OR location ILIKE $1
+                 OR description ILIKE $1
+                 OR requirements ILIKE $1)
+               AND user_id = $2`,
+            [`%${searchTerm}%`, userId],
+        );
+        return jobs.rows;
     }
 };
-const getJob = async (req, res) => {
-    try {
-        const user_id = req.params?.user_id;
-        const id = req.params?.id;
-        const job = await pool.query(
-            "SELECT * FROM jobs WHERE user_id = $1 AND id = $2",
-            [user_id, id]
-        );
-        if (job.rows.length === 0) {
-            res.status(404).json({
-                status: "error",
-                message: "Job not found",
-            });
-        } else {
-            res.json({
-                status: "success",
-                message: `Retrieved job with id ${id}`,
-                data: job.rows[0]
-            });
-        }
-    } catch (err) {
-        res.status(500).json({message: err.message});
+
+const getJob = async (userId, id) => {
+    const job = await pool.query(
+        "SELECT * FROM jobs WHERE user_id = $1 AND id = $2",
+        [userId, id]
+    );
+
+    if (job.rows.length === 0) {
+        throw new Error("Job not found");
+    } else {
+        return job.rows[0];
     }
 };
 
 const createJob = async (jobData, userId) => {
-    // insert into DB and return job
-    const job = await pool.query("INSERT INTO jobs (title, company, location, description, requirements, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    const newJob = await pool.query("INSERT INTO jobs (title, company, location, description, requirements, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
         [jobData.title, jobData.company, jobData.location, jobData.description, jobData.requirements, userId]);
-    return {
-        status: "success",
-        message: `Inserted job with id ${job.rows[0].id}`,
-        data: job.rows[0]
+    return newJob.rows[0];
+};
+
+const updateJob = async (jobData, userId, id) => {
+    const updatedJob = await pool.query(
+        "UPDATE jobs SET title = $1, company = $2, location = $3, description = $4, requirements = $5, is_applied = $6, updated_at = $7 WHERE user_id = $8 AND id = $9 RETURNING *",
+        [jobData.title, jobData.company, jobData.location, jobData.description, jobData.requirements, jobData.is_applied, jobData.updated_at, userId, id]
+    );
+
+    if (!updatedJob.rowCount) {
+        throw new Error("Job not found");
+    } else {
+        return updatedJob.rows[0];
     }
 };
 
-const updateJob = async (res, req) => {
-    try {
-        const user_id = req.params?.user_id;
-        const id = req.params?.id;
-        const {title, company, location, description, requirements} = req.body;
-        const job = await pool.query(
-            "UPDATE jobs SET title = $1, company = $2, location = $3, description = $4, requirements = $5 WHERE user_id = $6 AND id = $7 RETURNING *",
-            [title, company, location, description, requirements, user_id, id]
-        );
-        if (job.rows.length === 0) {
-            res.status(404).json({
-                status: "error",
-                message: "Job not found",
-            });
-        } else {
-            res.json({
-                status: "success",
-                message: `Updated job with id ${id}`,
-                data: job.rows[0]
-            });
-        }
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
-};
-
-//patch only is_applied
-const updateJobStatus = async (res, req) => {
-    try {
-        const user_id = req.params?.user_id;
-        const id = req.params?.id;
-        const {is_applied, updated_at} = req.body;
-        const job = await pool.query("UPDATE jobs SET is_applied = $1, updated_at = $2 WHERE user_id = $3 AND id = $4 RETURNING *", [is_applied, updated_at, user_id, id]);
-        if (job.rows.length === 0) {
-            res.status(404).json({
-                status: "error",
-                message: "Job not found",
-            });
-        } else {
-            res.json({
-                status: "success",
-                message: `Updated job with id ${id}`,
-                data: job.rows[0]
-            });
-        }
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
-};
-
-const deleteJob = async (user_id, id) => {
-    try {
-        await pool.query("DELETE FROM jobs WHERE user_id = $1 AND id = $2", [user_id, id]);
-        return "Job was deleted!";
-    } catch (err) {
-        throw err;
-    }
+const deleteJob = async (userId, id) => {
+    await pool.query("DELETE FROM jobs WHERE user_id = $1 AND id = $2", [userId, id]);
 };
 
 module.exports = {
@@ -138,5 +64,4 @@ module.exports = {
     createJob,
     updateJob,
     deleteJob,
-    updateJobStatus
 };
