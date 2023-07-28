d299655a0558b3b182da49dc22d05d2bb3dd56f2
enderta <endertanver@gmail.com>
2023-07-28 13:13:56 +0100

    Refactor code and add PropTypes in JobTracker
    The code in the JobTracker component was refactored to enhance clarity and maintainability. Some functions were rearranged for better cohesion. PropTypes were also introduced in the 'JobCard' component to validate props and improve debugging.

--- src/components/jobtracker/Cards.js
+++ src/components/jobtracker/Cards.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Button, Card} from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import Filters from './Filters';
 import Delete from "./Delete";
 import EditJob from "./EditJob";
@@ -10,14 +11,17 @@ const API_URL = 'http://localhost:5000/api';
 function Cards(props) {
     const [data, setData] = useState(props.data || []);
     const [search, setSearch] = useState('');
-    const userId = localStorage.getItem('user_id');
-    const token = localStorage.getItem('token');
-    const headers = {'Content-Type': 'application/json', Authorization: token};
 
+    const handleSearch = e => {
+        e.preventDefault();
+        setSearch(e.target.value);
+    }
 
-    function fetchJobs() {
+    const fetchJobs = () => {
+        const userId = localStorage.getItem('user_id');
+        const token = localStorage.getItem('token');
+        const headers = {'Content-Type': 'application/json', Authorization: token};
         const url = `${API_URL}/jobs/${userId}?search=${search}`;
-
         fetch(url, {method: 'GET', headers})
             .then((res) => res.json())
             .then((data) => setData(data.status === 'success' ? data.data : []))
@@ -25,13 +29,6 @@ function Cards(props) {
     }
 
     useEffect(fetchJobs, [search]);
-
-    function handleSearch(e) {
-        e.preventDefault();
-        setSearch(e.target.value);
-    }
-
-
 
     return (
         <div>
@@ -42,7 +39,9 @@ function Cards(props) {
             <div className="row" data-testid="cards-component">
                 {data && data.length > 0
                     ? data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
-                        .map((job) => <JobCard key={job.id} job={job} {...props} />)
+                        .map((job, index) => (
+                            <JobCard key={index} job={job} {...props} />
+                        ))
                     : []}
             </div>
         </div>
@@ -50,36 +49,21 @@ function Cards(props) {
 }
 
 function JobCard({job, handleCheck, dark}) {
-
-    function onMouseOver(e, title) {
-        e.target.style.cursor = 'pointer';
-        e.target.title = title;
-    }
-
-    /*  function getText(text) {
-          return text.split(' ').length > 3 ? `${text.split(' ').slice(0, 4).join(' ')} ...` : text;
-      }*/
-
     const [showEdit, setShowEdit] = useState(false);
 
     const handleEdit = () => setShowEdit(true);
     const handleEditClose = () => setShowEdit(false);
+
     return (
         <div className="col-md-3 mb-3">
             <Card data-testid="cards-component" style={{backgroundColor: dark ? '#070f23' : 'white'}}>
                 <Card.Body style={{height: '200px', width: '400px'}}>
                     <Card.Title>{job.title}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{job.company}</Card.Subtitle>
-                    {/*  <Card.Text
-                        onMouseOver={(e) => onMouseOver(e, job.description)}>{getText(job.description)}
-                    </Card.Text>*/}
+                    <Card.Text>{job.description}</Card.Text>
                     <Card.Text>{job.location}</Card.Text>
-                    {/* <Card.Text
-                        onMouseOver={(e) => onMouseOver(e, job.requirements)}>{getText(job.requirements)}
-                    </Card.Text>*/}
-                    <Card.Text>
-                        <IsApplied job={job}/>
-                    </Card.Text>
+                    <Card.Text>{job.requirements}</Card.Text>
+                    <Card.Text><IsApplied job={job}/></Card.Text>
                 </Card.Body>
                 <br/>
                 <Card.Footer>
@@ -92,6 +76,12 @@ function JobCard({job, handleCheck, dark}) {
             </Card>
         </div>
     );
+}
+
+JobCard.propTypes = {
+    job: PropTypes.object.isRequired,
+    handleCheck: PropTypes.func,
+    dark: PropTypes.bool
 }
 
 export default Cards;
