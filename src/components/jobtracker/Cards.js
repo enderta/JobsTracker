f931219a2ffd27324f1e91ba2f95003d25b648d8
enderta <endertanver@gmail.com>
2023-07-28 20:35:24 +0100

    Change date format in job tracker components
    Updated the date format from locale string to ISO string in the job tracker components for consistency and better handling of different date formats. Specifically, this change has been made in Edit

--- src/components/jobtracker/Cards.js
+++ src/components/jobtracker/Cards.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {Button, Card} from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import Filters from './Filters';
