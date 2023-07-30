bd2206ffe7f1cd846350c9317da14760246f3a71
enderta <endertanver@gmail.com>
2023-07-30 18:35:38 +0100

    Add new components and refactor Jumbo.js for better organization
    This commit involves adding new components for the job tracker feature. Components for the DarkModeButton, the JobCarousel, the LogInRedirect, the AddJobButton, and the JumbotronBackground are initialized. existing Jumbo.js file is refactored to import and use these new components for increased modularity and organization. In Jumbo.js, the function fetchJobs is moved inside the useEffect scope for better encapsulation, and replaced multiple instances where ShowModal's state was being toggled separately with a single handleModalToggle function. These changes aim to better manage state changes and make the codebase more maintainable.

---
+++ src/components/jobtracker/JobCarousel.js
@@ -0,0 +1,49 @@
+// JobCarousel.js
+import React from 'react';
+import {Button, Modal} from 'react-bootstrap';
+import {Carousel} from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+const JobCarousel = ({jobs, isOpen, openModal, closeModal, selectedJob}) => (
+    <Carousel
+        infiniteLoop
+        showThumbs={false}
+        showStatus={false}
+        showIndicators={false}
+        showArrows={false}
+        interval={3000}
+        centerMode={true}
+        centerSlidePercentage={100}
+        stopOnHover={true}
+        autoPlay={true}
+    >
+        {jobs.map((job) => (
+            <div key={job.id}>
+                <h3>{job.title}</h3>
+                {/* <h4>{job.company}</h4>*/}
+                <Button
+                    variant={'outline-warning'} onClick={() => openModal(job.id)}>View</Button>
+                <Modal show={isOpen} onHide={closeModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            <h4 style={{color: 'goldenrod'}}>{job.title}</h4>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h5 style={{color: 'darkgreen'}}>Company:{' '} {job.company}</h5>
+                        <h5 style={{color: 'darkgreen'}}>Location:{' '} {job.location}</h5>
+                        <h5 style={{color: 'darkgreen'}}>Description:{' '}{job.description}</h5>
+                        <h5 style={{color: 'darkgreen'}}>Requirements:{' '}{job.requirements}</h5>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={closeModal}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        ))}
+    </Carousel>
+);
+
+export default JobCarousel;
---
+++ src/components/jobtracker/LogInRedirect.js
@@ -0,0 +1,29 @@
+// LogInRedirect.jsx
+import React from 'react';
+import {Image} from 'react-bootstrap';
+
+const LogInRedirect = () => (
+    <h1 style={{color: 'goldenrod'}}>
+        <div>
+            <Image
+                src="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2016/10/camera-photo-lens-stock-images.jpg"
+                style={{position: "absolute", opacity: '0.5', height: "100%", width: "100%"}}
+            />
+            <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
+                <h1 style={{color: 'goldenrod'}}>Welcome to Job Tracker App </h1>
+                <h1 style={{color: 'goldenrod'}}>Please</h1>
+                <a href={'/login'} style={{textDecoration: "none"}}>
+                    <h1 style={{color: 'darkred'}}>Login</h1>
+                </a>
+                <h1 style={{color: 'goldenrod'}}>or</h1>
+                <h1 style={{color: 'goldenrod'}}>
+                    <a href={'/register'} style={{textDecoration: "none"}}>
+                        <h1 style={{color: 'darkgreen'}}>Register</h1>
+                    </a>
+                </h1>
+            </div>
+        </div>
+    </h1>
+);
+
+export default LogInRedirect;
