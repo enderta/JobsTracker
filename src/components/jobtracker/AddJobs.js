e84b22114801e2db16856788e8a64dd350efb97c
enderta <endertanver@gmail.com>
2023-08-02 00:03:12 +0100

    Add FormBuilder, JobForm components, and api calls separation.
    This commit introduces two new components FormBuilder and JobForm for enhancing modularity. The FormBuilder component is a reusable Form field that takes in different props to build different fields. The JobForm component shows all fields to input call for add jobs, initially present in the AddJobs.js file, has been abstracted into a separate file api.js. It allows better separation of concerns and makes maintenance and potential scaling easier.

--- src/components/jobtracker/AddJobs.js
+++ src/components/jobtracker/AddJobs.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import JobForm from './JobForm';
-import AddJobButton from "./AddJobButton";
+import useDarkMode from "./useDarkMode";
+import {Button} from "react-bootstrap";
 
 function AddJobs(props) {
     const currentDate = new Date().toISOString().split(' ').slice(0, 4).join(' ');
     const user_id = localStorage.getItem('user_id');
     const [showModal, setShowModal] = useState(false);
-
+    const [darkMode, setDarkMode] = useDarkMode();
     const handleOpenModal = () => setShowModal(true);
     const handleCloseModal = () => setShowModal(false);
     const initialJobState = {
@@ -26,7 +27,13 @@ function AddJobs(props) {
 
     return (
         <div className="container">
-            <AddJobButton handleOpen={handleOpenModal}/>
+            <Button
+                variant={darkMode ? 'outline-warning' : 'outline-dark'}
+                onClick={handleOpenModal}
+                data-testid={"addJobs"}
+            >
+                +
+            </Button>
 
             {showModal && (
                 <JobForm initialJobState={job} handleClose={handleCloseModal} handleChanges={handleChanges}/>
