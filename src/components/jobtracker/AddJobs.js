371f57e7f535625be3051036bbfad34e40dfc08f
enderta <endertanver@gmail.com>
2023-08-02 00:10:30 +0100

    Add FormBuilder, JobForm components, and api calls separation.
    This commit introduces two new components FormBuilder and JobForm for enhancing modularity. The FormBuilder component is a reusable Form field that takes in different props to build different fields. The JobForm component shows all fields to input call for add jobs, initially present in the AddJobs.js file, has been abstracted into a separate file api.js. It allows better separation of concerns and makes maintenance and potential scaling easier.

--- src/components/jobtracker/AddJobs.js
+++ src/components/jobtracker/AddJobs.js
@@ -1,15 +1,20 @@
 import React, {useState} from 'react';
-import JobForm from './JobForm';
-import useDarkMode from "./useDarkMode";
-import {Button} from "react-bootstrap";
+import {Button, Form, Modal} from 'react-bootstrap';
+
+const BASE_URL = 'https://jobapi-5ktz.onrender.com/api/jobs/createJob/';
+
+async function addJob(job) {
+    return await fetch(BASE_URL + job.user_id, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', Authorization: localStorage.getItem('token')},
+        body: JSON.stringify(job),
+    });
+}
 
 function AddJobs(props) {
     const currentDate = new Date().toISOString().split(' ').slice(0, 4).join(' ');
     const user_id = localStorage.getItem('user_id');
-    const [showModal, setShowModal] = useState(false);
-    const [darkMode, setDarkMode] = useDarkMode();
-    const handleOpenModal = () => setShowModal(true);
-    const handleCloseModal = () => setShowModal(false);
+
     const initialJobState = {
         title: '',
         company: '',
@@ -22,22 +27,81 @@ function AddJobs(props) {
         user_id,
     };
 
+// Initialize state
     const [job, setJob] = useState(initialJobState);
+    const onSubmitForm = async (e) => {
+        e.preventDefault();
+        try {
+            await addJob(job);
+            alert('Job added successfully');
+        } catch (err) {
+            console.error(err.message);
+        }
+        window.location.reload();
+    };
+
     const handleChanges = (e) => setJob({...job, [e.target.name]: e.target.value});
 
     return (
         <div className="container">
-            <Button
-                variant={darkMode ? 'outline-warning' : 'outline-dark'}
-                onClick={handleOpenModal}
-                data-testid={"addJobs"}
-            >
-                +
-            </Button>
+            <Modal show={props.show} onHide={props.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        <h3 style={{color: "black"}}>Add Job</h3>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form style={{color: "black"}}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Title" name="title"
+                                          data-testid="Enter Title"
+                                          value={job.title}
+                                          onChange={handleChanges}/>
+                        </Form.Group>
 
-            {showModal && (
-                <JobForm initialJobState={job} handleClose={handleCloseModal} handleChanges={handleChanges}/>
-            )}
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Company</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Company" name="company"
+                                          data-testid="Enter Company"
+                                          value={job.company}
+                                          onChange={handleChanges}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Location</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Location" name="location"
+                                          value={job.location}
+                                          data-testid="Enter Location"
+                                          onChange={handleChanges}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Description" name="description"
+                                          value={job.description}
+                                          data-testid="Enter Description"
+                                          onChange={handleChanges}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Requirements</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Requirements" name="requirements"
+                                          value={job.requirements}
+                                          data-testid="Enter Requirements"
+                                          onChange={handleChanges}/>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.handleClose}>
+                        Close
+                    </Button>
+                    <div>
+                        <Button variant="primary" onClick={onSubmitForm}
+                                data-testid={"submit-button"}>
+                            Save Changes
+                        </Button>
+                    </div>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
