fec77ad54ee4f3b93530206ad6e40d83807b041c
enderta <endertanver@gmail.com>
2023-08-11 21:19:44 +0100

    Add "Scroll to Top" button and modify "Cards" component
    Integrated a "Scroll to top" feature on the Jobtracker app page for improved user navigation, especially during long scrolls. This necessitated creating new css file 'scrollToTop.css' and 'ScrollToTop.js' files. The scroll to top feature detects whether the user has scrolled down and accordingly shows the button on better structuring and ordering of components in 'Jumbo.js'.

--- src/components/jobtracker/IsApplied.js
+++ src/components/jobtracker/IsApplied.js
@@ -1,14 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-const API_URL = 'https://jobapi-5ktz.onrender.com/api';
-const IsApplied = (props) => {
-    const userId = localStorage.getItem('user_id');
-    const token = localStorage.getItem('token');
-    const [isApplied, setIsApplied] = useState(props.job.is_applied);
-    const headers = {'Content-Type': 'application/json', Authorization: token};
+
+const API_URL = 'http://localhost:5000/api/jobs';
+const headers = {'Content-Type': 'application/json', Authorization: localStorage.getItem('token')};
+const currentDate = new Date().toISOString().split(' ').slice(0, 4).join(' ');
+const userId = localStorage.getItem('user_id');
+
+const isApplied = (props) => {
     const data = props.job;
-    const currentDate = new Date().toISOString();
-
     const baseJobData = {
         title: data.title,
         company: data.company,
@@ -16,39 +15,45 @@ const IsApplied = (props) => {
         description: data.description,
         requirements: data.requirements,
         posted_at: currentDate,
-        is_applied: isApplied,
+        is_applied: data.is_applied,
         user_id: userId,
     };
 
     const handleCheck = async (id, isApplied) => {
-        const url = `${API_URL}/jobs/${userId}/${id}`;
-        const updatedJobData = {...baseJobData, is_applied: true, updated_at: currentDate};
-        const body = JSON.stringify(updatedJobData);
+        const url = `${API_URL}/${userId}/${id}`;
 
-        try {
-            const response = await fetch(url, {method: 'PUT', headers, body});
-            const data = await response.json();
-            if (data.status === 'success') {
-                setIsApplied(true)
+        if (isApplied === false) {
+            const updatedJobData = {...baseJobData, is_applied: true, updated_at: currentDate};
+            const body = JSON.stringify(updatedJobData);
+
+            try {
+                const response = await fetch(url, {method: 'PUT', headers, body});
+                const data = await response.json();
+
+                if (data.status === 'success') {
+                    window.location.reload();
+                }
+            } catch (error) {
+                console.log(error);
             }
-        } catch (error) {
-            console.log(error);
         }
     };
-
     return (
         <div>
-            <h6
-                cy-data="applied-at"
-                onClick={() => handleCheck(data.id, isApplied)}
-                style={{color: isApplied ? 'forestgreen' : 'goldenrod',}}
-            >
-                {isApplied
-                    ? `Applied At: ${new Date(data.updated_at).toISOString().split("T")[0]}`
-                    : 'If you applied, click here!'}
-            </h6>
+            <p>
+        <span
+            cy-data="applied-at"
+            onClick={() => handleCheck(data.id, data.is_applied)}
+            style={{color: data.is_applied ? 'forestgreen' : 'goldenrod',}}
+        >
+            {props.job.is_applied
+                ? `Applied At: ${new Date(data.updated_at).toISOString().split("T")[0]}`
+                : 'If you applied, click here!'}
+        </span>
+            </p>
+
         </div>
     );
 };
 
-export default IsApplied;
+export default isApplied;
