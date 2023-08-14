7edc6d0426135cbabe135cb54988e6016f95e207
enderta <endertanver@gmail.com>
2023-08-14 18:00:00 +0100

    "Repositioned functional buttons for better UX"
    Changes were made to the positioning of AddJobButton, DarkModeButton, and LogOut button, making them fixed at specific locations on the screen. Preferably, AddJobButton and DarkModeButton are at the top left and right, respectively, while LogOut button is at the bottom left

--- src/components/jobtracker/AddJobButton.js
+++ src/components/jobtracker/AddJobButton.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Button} from 'react-bootstrap';
 
 const AddJobButton = ({darkMode, handleShow}) => (
-    <div style={{position: "fixed", top: "10px", left: "10px"}}>
-    <Button
-        variant={darkMode ? 'outline-warning' : 'outline-dark'}
-        onClick={handleShow}
-        data-testid={"addJobs"}
-    >
-        +
-    </Button>
+    <div style={{position: "fixed", top: "10px", left: "10px", zIndex: 1000}}>
+        <Button
+            variant={darkMode ? 'outline-warning' : 'outline-dark'}
+            onClick={handleShow}
+            data-testid={"addJobs"}
+        >
+            +
+        </Button>
     </div>
 );
 
--- src/components/jobtracker/Cards.js
+++ src/components/jobtracker/Cards.js
@@ -5,13 +5,10 @@ import Filters from './Filters';
 import JobCard from './JobCard';
 import useFetchJobs from './useFetchJobs';
 
-function Cards(props) {
+function Cards({dark}) {
     const [search, setSearch] = useState('');
     const [limit, setLimit] = useState(3);
-
-    const jobsInfo = useFetchJobs(search, limit);
-    const data = jobsInfo[0]
-    const jobNumber = jobsInfo[1]
+    const [data, jobNumber] = useFetchJobs(search, limit);
 
     const handleSearch = useCallback((e) => {
         e.preventDefault();
@@ -19,47 +16,67 @@ function Cards(props) {
     }, []);
 
     const sortedJobs = useMemo(() => {
-        if (!jobNumber || jobNumber.length === 0) return [];
+        if (!jobNumber || jobNumber.length === 0) {
+            return [];
+        }
         return [...data].sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
     }, [data]);
+
+    const renderOptions = () => {
+        const options = [];
+
+        for (let i = 3; i <= jobNumber.length && i <= 12; i += 3) {
+            options.push(<option value={i.toString()} key={i}>{i}</option>);
+        }
+
+        if (jobNumber.length > 12 || jobNumber.length % 3 !== 0) {
+            options.push(<option value="0" key="all">All</option>);
+        }
+
+        return options;
+    };
+
     return (
         <div>
             <div style={{marginTop: '10px', marginBottom: '10px'}}>
                 <Filters data={data} value={search} handleSearch={handleSearch}/>
             </div>
             <div>
-                <h4>Total Jobs: {jobNumber.length}</h4>
+                <h4>
+                    {jobNumber.length === 0 ? 'No job click + button to add job'
+                        : jobNumber.length === 1
+                            ? '1 job' : `${jobNumber.length} jobs`}
+                </h4>
             </div>
             <div style={{marginTop: '10px', marginBottom: '10px'}}>
-                <h4 style={{marginRight: '10px'}}>Show</h4>
-                <Form.Select
-                    aria-label="Default select example"
-                    style={{width: '100px'}} // adjust width as needed
-                    value={limit}
-                    onChange={e => setLimit(parseInt(e.target.value))}
-                >
-                    {jobNumber.length < 3 ? <option value="0">All</option> : null}
-                    {jobNumber.length === 3 ? (<option value="3">3</option>) : null}
-                    {jobNumber.length >= 4 && jobNumber.length < 6 ? (<option value="0">All</option>) : null}
-                    {jobNumber.length === 6 ? (<option value="6">6</option>) : null}
-                    {jobNumber.length >= 7 && jobNumber.length < 9 ? (<option value="0">All</option>) : null}
-                    {jobNumber.length === 9 ? (<option value="9">9</option>) : null}
-                    {jobNumber.length >= 10 && jobNumber.length < 12 ? (<option value="0">All</option>) : null}
-                    {jobNumber.length === 12 ? (<option value="12">12</option>) : null}
-                    {jobNumber.length > 13 ? (<option value="0">All</option>) : null}
-                </Form.Select>
+                {jobNumber.length > 3 ? (
+                    <>
+                        <h4 style={{marginRight: '10px'}}>Show</h4>
+                        <Form.Select
+                            aria-label="Default select example"
+                            style={{width: '100px'}} // adjust width as needed
+                            value={limit}
+                            onChange={(e) => setLimit(parseInt(e.target.value))}
+                        >
+                            {renderOptions()}
+                        </Form.Select>
+
+                        <div className="row" data-testid="cards-component">
+                            {sortedJobs.map((job) => (
+                                <JobCard key={job.id} job={job} dark={dark}/>
+                            ))}
+                        </div>
+                    </>
+                ) : (
+                    <div className="row" data-testid="cards-component">
+                        {data.map((job) => (
+                            <JobCard key={job.id} job={job} dark={dark}/>
+                        ))}
+                    </div>
+                )}
             </div>
-
-            <div className="row" data-testid="cards-component">
-                {sortedJobs.map((job) => (
-                    <JobCard key={job.id} job={job} dark={props.dark}/>
-                ))}
-            </div>
-
-
         </div>
     );
 }
-
 
 export default Cards;
