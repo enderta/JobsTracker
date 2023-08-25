9495c13c823096eed4d3c04d0f0364e7e77b5621
enderta <endertanver@gmail.com>
2023-08-25 16:56:18 +0100

    Reduce jumbotron height and refactor news ticker display
    Adjusted the height of the `JumbotronBackground` for better visual balance within the layout of JobTracker. In addition, the margin settings in `Jumbo.js` have been revised for better alignment. The news ticker in `NewsTicker.css` and `NewsTicker.js` has been modified to slow down the display speed for better readability, and pause the ticker animation on hover for user-friendly interaction. Also added functionality to fetch news headlines from an external API source, displaying headline title with a corresponding link.

--- src/components/jobtracker/NewsTicker.js
+++ src/components/jobtracker/NewsTicker.js
@@ -1,50 +1,74 @@
 import React, {useEffect, useState} from 'react';
-import './NewsTicker.css'; // Import the CSS file for styling
+import './NewsTicker.css';
 
-const newsHeadlines = [
-    "Breaking News: New discovery in space!",
-    "Economy grows by 3% in the last quarter.",
-    "Health officials issue new COVID-19 guidelines.",
-    // Add more news headlines here
-];
+const defulatNews = [
 
-const NewsTicker = () => {
+    {
+        "source": {
+            "id": "engadget",
+            "name": "Engadget"
+        },
+        "author": null,
+        "title": "Blink's Outdoor 4 security camera offers sharper video day and night - Engadget",
+        "description": "It adds person detection to the wire-free model..",
+        "url": "https://www.engadget.com/blinks-outdoor-4-security-camera-offers-sharper-video-day-and-night-140100728.html",
+        "urlToImage": "https://s.yimg.com/uu/api/res/1.2/456.A.IkACMFr3LjqVZgSQ--~B/Zmk9ZmlsbDtoPTYzMDtweW9mZj0wO3c9MTIwMDthcHBpZD15dGFjaHlvbg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2023-08/705e91a0-40dd-11ee-92ff-ea725247a4d8.cf.jpg",
+        "publishedAt": "2023-08-24T14:00:47Z",
+        "content": "Blink is today announcing its latest external security camera, the Blink Outdoor 4, which boasts better image quality, improved low-light sensitivity and a wider field of view. For the quality claims… [+1383 chars]"
+    },
+    {
+        "source": {
+            "id": "the-verge",
+            "name": "The Verge"
+        },
+        "author": "Tom Warren",
+        "title": "As one Destiny 2 saga ends, Bungie prepares for the future - The Verge",
+        "description": "Destiny 2: The Final Shape arrives in February to mark a closure of the light and darkness saga. Bungie is now looking at the future of Destiny.",
+        "url": "https://www.theverge.com/23844068/destiny-2-the-final-shape-joe-blackburn-interview",
+        "urlToImage": "https://cdn.vox-cdn.com/thumbor/nCbPVRPxiSUj5hf2zvPw61b9Ck0=/0x0:3840x2160/1200x628/filters:focal(1920x1080:1921x1081)/cdn.vox-cdn.com/uploads/chorus_asset/file/24870278/2023_Final_Shape_Reveal_Press_Kit_Standard_Key_Art_16x9.jpg",
+        "publishedAt": "2023-08-24T14:00:01Z",
+        "content": "As one Destiny 2 saga ends, Bungie prepares for the future\r\nAs one Destiny 2 saga ends, Bungie prepares for the future\r\n / We sat down with game director Joe Blackburn to talk about the future of the… [+10244 chars]"
+    }]
+
+
+const fetchArticles = async () => {
+    const response = await fetch('https://newsapi-fn68.onrender.com/news');
+    const data = await response.json();
+    if (data.articles.length === 0) return defulatNews;
+    return data.articles;
+};
+
+const useNewsTicker = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [newsHeadlines, setNewsHeadlines] = useState([])
-
-    const getNewsHeadlines = async () => {
-        const response = await fetch('https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=895b50e9e60744729f4308b9e8ff9aae');
-        const data = await response.json();
-        const headlines = data.articles;
-        headlines.length !== 0 ? setNewsHeadlines(headlines) : setNewsHeadlines(["No news available"])
-    }
-    console.log(newsHeadlines)
-
+    const [articles, setArticles] = useState([]);
 
     useEffect(() => {
-        getNewsHeadlines();
-    }, [])
+        fetchArticles().then(articles => setArticles(articles));
+    }, []);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentIndex(currentIndex => (currentIndex + 1) % newsHeadlines.length);
-        }, 8000);  // Displays next news after 6 seconds
+            setCurrentIndex(currentIndex => (currentIndex + 1) % articles.length);
+        }, 8000);
         return () => clearInterval(interval);
-    }, [newsHeadlines.length]);
+    }, [articles]);
 
+    return {articles, currentIndex};
+};
+
+const NewsTicker = () => {
+    const {articles, currentIndex} = useNewsTicker();
 
     return (
         <div className="news-ticker-container">
             <div className="news-ticker">
-                {newsHeadlines.map((headline, index) => (
+                {articles.map((headline, index) => (
                     <div
                         key={index}
-                        className={`ticker-item ${index === currentIndex ? 'active' : ''}`}
-                    >
+                        className={`ticker-item ${index === currentIndex ? 'active' : ''}`}>
                         <a className="myLinkStyle" href={headline.url} target="_blank" rel="noreferrer">
                             {`${headline.source.Name}: ${headline.title.replace(` - ${headline.source.Name}`, '')}`}
                         </a>
-
                     </div>
                 ))}
             </div>
